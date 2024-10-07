4771dae41fdfa3b9eeab0cce09ef415d216bc909
User <User@DESKTOP-BC53N1O>
2024-10-07 20:53:19 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,23 @@
+﻿
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  
+        const int m_per_km{ 1000 };    
+
+        int meters{};
+        std::cout << "Enter a number of metres: ";
+        std::cin >> meters;   
+
+        const int kilometers{ meters / m_per_km };        
+        const int remaining_meters{ meters % m_per_km };  
+
+        std::cout << meters << " meters = " << kilometers << " kilometers and " << remaining_meters << " meters.";
+}
+
+
